5d327f85abcde3b48bed37990e408e6290539c15
Silva Rafael (CaP/ETS) <srv2ca@bosch.com>
2022-05-27 11:31:11 -0300

    Finalizado para entrega

--- Backend/techman/settings.py
+++ Backend/techman/settings.py
@@ -82,19 +82,19 @@ WSGI_APPLICATION = 'techman.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db_techman',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': 3300
-    }
-    
     # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'db_techman',
+    #     'USER': 'root',
+    #     'PASSWORD': 'root',
+    #     'HOST': 'localhost',
+    #     'PORT': 3300
     # }
+    
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
